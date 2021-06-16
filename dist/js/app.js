7d6c7248a87db2fc9f523ec184821c87dbb0b1ea
domingo79 <andria140779@yahoo.it>
2021-06-16 15:52:10 +0200

    generate cards con vue

--- dist/js/app.js
+++ dist/js/app.js
@@ -11,7 +11,18 @@ var app = new Vue({
   el: '#app',
   data: {
     phone: '+1(305) 1234-5678',
-    email: 'hello@example.com'
+    email: 'hello@example.com',
+    businessAria: [{
+      name: 'card_tecnology',
+      image: './dist/img/gallery-3.jpg',
+      title: 'Technology',
+      text: "Focused on developing techology solutions adapted to out client's needs."
+    }, {
+      name: 'card_transport',
+      image: './dist/img/gallery-5.jpg',
+      title: 'Transport',
+      text: "We develop operational strategies to improve logical efficiency."
+    }]
   }
 });
 
