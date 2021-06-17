ac16c6c6ad4b1ed0b9d05aba9e1049b6fe83dfb6
domingo79 <andria140779@yahoo.it>
2021-06-17 17:19:10 +0200

    sezione send message

--- dist/js/app.js
+++ dist/js/app.js
@@ -12,6 +12,7 @@ var app = new Vue({
   data: {
     phone: '+1(305) 1234-5678',
     email: 'hello@example.com',
+    address: 'Main Avenue, 987',
     businessAria: [{
       name: 'card_tecnology',
       image: './dist/img/gallery-3.jpg',
